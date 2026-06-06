a2e7330515c2f38ef7f518676552872f4e718598
seannxh <nohsean6@gmail.com>
2026-06-06 18:08:23 -0500

    lambda function practice

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,34 @@
 import jdk.internal.org.jline.utils.AttributedStringBuilder.append
+import java.util.function.Predicate
 
 fun main() {
     println("Enter a String: ")
     val input = readln()
 
-    val lettersOnly = input.filter {it.isLetter()}
-    println(lettersOnly)
-
-    println("Enter the size of the array")
-    var i = 0
-    val input2 = readln().toIntOrNull() ?: 0
-    val result = mutableListOf<Int>()
-    while (i < input2) {
-        println("What number do you want to add to the array?")
-        val input3 = readln().toIntOrNull() ?: 0
-        result.add(input3)
-        i++
-
+    val favoriteNumbers = intArrayOf(1, 2, 3, 69)
+    val evenNumbers = favoriteNumbers.filter { it % 2 == 0 }
+    val evenNumbers2 = favoriteNumbers.map{
+        it * it
     }
-    println(result.filter { it % 2 == 0 })
+
+    println(evenNumbers2)
+    val lambda: (Char) -> Boolean = {
+        it.isLetter()
+    }
+    val lettersOnly = input.myFilter{
+        isLetter()
+    }
+    println(lettersOnly)
+    println(evenNumbers)
+}
+
+fun String.myFilter(predicate: Char. () -> Boolean): String {
+
+    return buildString {
+        for(char in this@myFilter){
+            if (predicate(char)){
+                append(char)
+            }
+        }
+    }
 }
