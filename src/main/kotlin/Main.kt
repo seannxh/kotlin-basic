842c5c7a349cc6dfa63d57ea805d3ee212daf69a
seannxh <nohsean6@gmail.com>
2026-06-06 16:07:16 -0500

    Reverse loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,20 @@
+import jdk.internal.org.jline.utils.AttributedStringBuilder.append
+
 fun main() {
-//    println("How many numbers?")
-//    val userNumber = readln().toIntOrNull() ?: run {
-//        println("Invalid input")
-//        return
+//    val results = mutableListOf<Int>()
+//    val numbers = listOf(1, 2, 3)
+//    for (number in numbers) {
+//        val result = number * number
+//        results.add(result)
 //    }
-//
-//    for (i in 1..5) {
-//        println("Numbers $i")
-//    }
-//
-//    val list = mutableListOf<Int>()
-//
-//    for (i in 1..5){list.add(i)}
-//
-//    list.forEach {println(it * 2)}
-//
-//    repeat(3) { index ->
-//        list.add(index * 3)
-//    }
-//    println(list.joinToString(" | "))
-
-
-    do {
-        println("Enter a Number: ")
-        val input = readln().toIntOrNull()
-        println("Your number is $input")
-    } while (input == null)
-
-    val list = listOf("Sean", "John", "Mike")
-    list.forEachIndexed { index, name ->
-        println("$index: $name")
+//    println(results)
+    println("Enter a string")
+    val input = readln()
+    //Reversing String
+    val finalString = buildString {
+        for (i in input.lastIndex downTo  0) {
+            append(input[i])
+        }
     }
-
-    val map = mapOf("name" to "Sean", "City" to "Houston", "Job" to "Affirm")
-    for((key, value) in map){
-        println("$key = $value")
-    }
-
+    println(finalString)
 }
--- src/main/kotlin/Notes.kt
+++ src/main/kotlin/Notes.kt
@@ -166,3 +166,41 @@
 //    }
 //
 //    println("Numbers: $numbersList")
+
+//    println("How many numbers?")
+//    val userNumber = readln().toIntOrNull() ?: run {
+//        println("Invalid input")
+//        return
+//    }
+//
+//    for (i in 1..5) {
+//        println("Numbers $i")
+//    }
+//
+//    val list = mutableListOf<Int>()
+//
+//    for (i in 1..5){list.add(i)}
+//
+//    list.forEach {println(it * 2)}
+//
+//    repeat(3) { index ->
+//        list.add(index * 3)
+//    }
+//    println(list.joinToString(" | "))
+
+//
+//do {
+//    println("Enter a Number: ")
+//    val input = readln().toIntOrNull()
+//    println("Your number is $input")
+//} while (input == null)
+//
+//val list = listOf("Sean", "John", "Mike")
+//list.forEachIndexed { index, name ->
+//    println("$index: $name")
+//}
+//
+//val map = mapOf("name" to "Sean", "City" to "Houston", "Job" to "Affirm")
+//for((key, value) in map){
+//    println("$key = $value")
+//}
